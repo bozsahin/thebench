2a37ed2b461749f2dcbf1a7d55c3c702def53e0a
Cem Bozsahin <cem.bozsahin@gmail.com>
2022-10-12 19:22:37 +0300

    bayi yara and saturday not parsed

--- examples/pftl.mtest.lisp
+++ examples/pftl.mtest.lisp
@@ -56,13 +56,13 @@
 (65s (ano ang sinabi "ni pedro" na binili "ni linda"))
 (69s (huhugasan ko at pupunasan mo ang_mga_pinggan))
 ;; below are experimental
-(l1 (persuade Harry to buy and Barry to sell))
-(l2 (persuade to go to London John))
-(l3 (want to go and promise to accompany to London)) ;; think of who didja ...
-(l4 (folded rug over)) ; and curtains under, the painting
-(d1 (what you can)) ;; and what you must not count on
-(d2 (what Terry asked)) ;; and what Mary promised Harry to read
-(s2 (gave without reading)) ;; the book I gave without reading and lent wout stamping to Harry
+;(l1 (persuade Harry to buy and Barry to sell))
+;(l2 (persuade to go to London John))
+;(l3 (want to go and promise to accompany to London)) ;; think of who didja ...
+;(l4 (folded rug over)) ; and curtains under, the painting
+;(d1 (what you can)) ;; and what you must not count on
+;(d2 (what Terry asked)) ;; and what Mary promised Harry to read
+;(s2 (gave without reading)) ;; the book I gave without reading and lent wout stamping to Harry
 ))
 
 (defun  test-ders()
