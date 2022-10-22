ad0c6d533082ec6b2a80a58bf73d1d622faa1f5c
https://github.com/bozsahin <cem.bozsahin@gmail.com>
2022-10-22 15:21:38 +0300

    first testing of training works ok; on to scripted training

--- examples/model-with-workflow/testsuite.lisp
+++ examples/model-with-workflow/testsuite.lisp
@@ -11,35 +11,28 @@
   (john knows mary loves john)
   ))
 
-(defun prepare-supervision ()
-  (make-supervision "g1"))
-
 (defun train-noqnoc ()
-  (prepare-supervision)   ; creates g1.sup from g1.supervision --optional if you already have .sup file
-  (lg "g1")               ; makes and loads the .ccg.lisp file  from .ccg
-  (um "g1" 10 1.0 1.0 :load t)  ; updates g1.ccg.lisp after loading it, using g1.sup
-  (st)			; shows training
-  (save-training "g1-updated.ccg.lisp")
+  (load_bin "g1.g.bin")        
+  (update-model "g1.g.bin" 10 1.0 1.0 :load t)  ; updates g1.ccg.lisp after loading it, using g1.sup
+  (show-training)
   )
 
 (defun train-noqnoc-xp ()
   "in case we want to extrapolae the gradient"
-  (prepare-supervision)   ; creates g1.sup from g1.supervision --optional if you already have .sup file
-  (lg "g1")               ; makes and loads the .ccg.lisp file  from .ccg
+  (load_bin "g1.g.bin")               ; makes and loads the .ccg.lisp file  from .ccg
   (nf-parse-on)           ; use normal form parsing
-  (umxp "g1" 1.0 1.0 :load t)  ; update using extrapolation (fixed to 4 iterations)
+  (update-model-xp "g1.g.bin" 1.0 1.0 :load t)  ; update using extrapolation (fixed to 4 iterations)
   (show-training-xp)	    ; shows training using extrapolation
-  (save-training "g1-updated-xp.ccg.lisp")
   )
 
 (defun test-noqnoc (g)
-  (lg g) ; this is the trained grammar if you called train-noqnoc before
+  (load_bin "g1.g.bin")
   (format t "~%---------~%Testing the ~A grammar" g)
   (dolist (ex *db*)
     (format t "~2%======~%all derivations of ~A without ranking~%" ex)
-    (p ex)
-    (ders)
+    (cky_analyze ex)
+    (cky_show_analysis)
     (format t "~2%======~%all derivations of ~A with ranking~%" ex)
-    (rank ex)
-    (status) ; shows the most likely LF
+    (cky_rank ex)
+    (cky_show_ranking) ; shows the most likely LF
     ))
