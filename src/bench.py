dee1960d37e83818c5a835fc61c4e24792a28ee0
bozsahin <cem.bozsahin@gmail.com>
2025-05-16 11:15:45 +0300

    MG terminology in displays; start menu update

--- src/bench.py
+++ src/bench.py
@@ -43,8 +43,8 @@ if not os.path.exists(_tmp): # someone deleted it, recreate
 _home=os.getcwd()
 _prompt = '/'+_overscore+'\ ' # the pagoda
 _online = False               # parser output control
-_version = '2.1'
-_vdate = 'November 14, 2024'
+_version = '2.1.1'
+_vdate = 'May 16, 2025'
 # 3 built-in extensions of MG
 _binext = '.src'              # lisp code extension
 _supext = '.sup'              # native format extension for supervision files
