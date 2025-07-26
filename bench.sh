6e2ea9be0d2f27441907a5d536904bec0490df1e
bozsahin <cem.bozsahin@gmail.com>
2025-07-26 13:08:31 +0300

    coded Roswell install into installer

--- bench.sh
+++ bench.sh
@@ -11,6 +11,7 @@ BENCH_HOMEP="$HOME/.thebenchhome" # this file will contain the
 BENCH_HISTORY="$HOME/.thebenchhistory" # py saves commands in it internally
 BHF='' # the dir pointed by BENCH_HOMEP; initially none
 ULB="$HOME/.local/bin" # where thebench binary goes -- we dont need sudo for this
+UL="$HOME/.local"      # where the local sbcl and its repo goes
 BENCHBIN='thebench'  # this is the name of the binary 'bench' is shorter but it might name-collide
 TMPB='/var/tmp/thebench'  # where the temporary files of analysis and training go
 NOPY=
@@ -92,19 +93,32 @@ if [ $THEBENCHCOMMAND == uninstall ]; then
 	exit 0
 fi
 
-# from now on we also need python and pip
+# from now on we are adding/changing stuff in .local and .local/bin; we also need python and pip
 #
+if [ ! -d $UL ]; then
+	LOG+="\n-There is no $UL in your system; creating one..."
+        mkdir $UL
+        chmod u+rwx $UL
+fi
+if [ ! -d $ULB ]; then
+	LOG+="\n-There is no $ULB in your system; creating one..."
+        mkdir $ULB
+        chmod u+rwx $ULB
+fi
 if [ $THEBENCHCOMMAND == reset ]; then
 	$THEBENCHPIP install cl4py
 	$THEBENCHPIP install sly
         $THEBENCHPIP install prompt_toolkit
-	echo "$THEBENCHPYTHON `pwd`/src/bench.py" | sudo tee "$ULB/$BENCHBIN" 
+	echo "$THEBENCHPYTHON `pwd`/src/bench.py" | tee "$ULB/$BENCHBIN" 
+	echo $LOG
 	echo "TheBench binary thebench is set to execute: `cat $ULB/$BENCHBIN`"
 	echo "TheBench is reset to use $THEBENCHPYTHON"
 	chmod u+x $ULB/$BENCHBIN
 	exit 0
 fi
+
 # If we've come this far, we are installing
+#
 if [ $THEBENCHCOMMAND == install ]; then
 	if [ -e $BENCH_HOMEP ]; then
 		echo "You have TheBench installed at: `cat $BENCH_HOMEP`"
@@ -119,60 +133,29 @@ if [ $THEBENCHCOMMAND == install ]; then
 	#check if SBCL need installing-- SBCL is the standard  lisp for cl4py module
 	#there is a pecking order of packagers, in case you have more than one
 	if [ ! `command -v sbcl` ]; then
-		packager=
-		install=install
-		if [ `command -v dnf` ]; then
-			packager=dnf
-		fi
-		if [ `command -v yum` ]; then
-	  		packager=yum   
-	  		# open library space of yum
-	  		$SUDO yum $install yum-utils
-	  		$SUDO yum-config-manager --enable \*
-		fi
-		if [ `command -v apt-get` ]; then
-			packager=apt-get
-			# open library space of apt-get and refresh
-			$SUDO add-apt-repository universe
-			$SUDO apt-get update
-		fi
-		if [ `command -v pacman` ]; then
-			packager=pacman
-			install='-S'
-		fi
-		if [ `command -v brew` ]; then
-    			packager=brew
-			SUDO=
-		fi
-		if [ "$packager" ]; then
-			LOG+="\n-You have an installer ($packager) for standard packages"
-			$SUDO $packager $install sbcl
-			LOG+="\n-sbcl is downloaded and installed"
-		else
-			LOG+="\n-apt-get, dnf, pacman, yum or brew not found. I leave Common Lisp handling to you."
-		fi
+                cd $UL
+                git clone https://github.com/roswell/roswell.git
+                cd roswell
+		sh bootstrap
+		./configure --prefix=$UL
+		make
+		make install
+		ros install sbcl 
+		printf '%s\n' "export PATH=$PATH" >> $HOME/.bashrc
+		LOG+="\n-Locally installing Roswell git repo in $UL"
+		LOG+="\n-Locally installing sbcl via  Roswell in $ULB"
+		LOG+="ros install sbcl  installs sbcl and internally adds its path ~/.local/bin to PATH"
+		LOG+='\n-NB. your new PATH is augmented with the previous one at the end of ~/.bashrc to make it permanent'
 	else
-  		LOG+="\n-Local sbcl is set for tool use"
+  		LOG+="\n-Local sbcl is adopted for thebench use"
 	fi
 	$THEBENCHPIP install cl4py
 	$THEBENCHPIP install sly
         $THEBENCHPIP install prompt_toolkit
 	LOG+="\n-Three $THEBENCHPYTHON libraries set for TheBench use: cl4py, sly, prompt_toolkit"
-	if [ ! -d $ULB ]; then
-		LOG+="\n-There is no $ULB in your system; creating one..."
-                LOG+="\n-Here is your PATH variable's contents: $PATH"
-                LOG+="\n-If $ULB is not in it, add it at the end, separating it with ':'"
-                LOG+="\n-It is usually set in the .bashrc file in your home directory."
-                sudo mkdir $ULB
-		sudo chmod u+rwx $ULB
-	else
-                LOG+="\n-Here is your PATH variable's contents: $PATH"
-                LOG+="\n-If $ULB is not in it, add it at the end, separating it with ':'"
-                LOG+="\n-PATH is usually set in the .bashrc file in your home directory."
-        fi
-	echo "$THEBENCHPYTHON `pwd`/src/bench.py" | sudo tee "$ULB/$BENCHBIN" # sudo must be on write
+	echo "$THEBENCHPYTHON `pwd`/src/bench.py" | tee "$ULB/$BENCHBIN" 
 	LOG+="\n-TheBench binary thebench is set to execute: `cat $ULB/$BENCHBIN`"
-        sudo chmod ugo+x "$ULB/$BENCHBIN"  # to call bench from anywhere
+        chmod ugo+x "$ULB/$BENCHBIN"  # to call bench from anywhere
 	echo "`pwd`" > $BENCH_HOMEP   # repo pointer saved at home dir as a dot file
 	echo "" > $BENCH_HISTORY      # command history saved at home dir as a 
 	                              #     dot file (py refers to it internally)
@@ -186,13 +169,11 @@ if [ $THEBENCHCOMMAND == install ]; then
 	LOG+="\n\n-thebench install: COMPLETED"
 	LOG+="\n-The log is saved in file $LOGFILE"
 	echo "The log is saved in file $LOGFILE"
-        LOG+="\n-PLEASE CHECK IT IF THE INSTALLER ASKED FOR SOME ADDITIONAL ACTION"
-        echo "PLEASE CHECK IT IF THE INSTALLER ASKED FOR SOME ADDITIONAL ACTION"
 	LOG+="\n========================================================="
 	echo -e $LOG > $LOGFILE
 	echo -e $LOG
        	exit 0
 else
 	echo "Unknown action. Exiting without action"
-	exit 0
+	exit -1
 fi
