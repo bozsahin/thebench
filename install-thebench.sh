eff511340c52ee284d1eb1c8b5b93f693ef1a201
Cem Bozsahin <cem.bozsahin@gmail.com>
2025-11-18 15:32:53 +0300

    thebench command ensures bash; avoiding shell conflicts

--- install-thebench.sh
+++ install-thebench.sh
@@ -1,108 +1,108 @@
-#!/bin/bash 
-# cem bozsahin 2025
-BENCH_HOMEP="$HOME/.thebenchhome" # this file will contain the pointer to TheBench repo in full path
-BENCH_HISTORY="$HOME/.thebenchhistory" # bench.py will save commands in it internally
-BENCHBIN='thebench'  # this is the name of the binary; 'bench' is shorter but its name collides
-TMPB='/var/tmp/thebench'  # where the temporary files of analysis and training go
-PY="3.11"                 # specific python for thebench
-LOGFILE='/var/tmp/thebench-install.log' # goes there to avoid .gitignore in repo directory
-LOG="========================================================="
-LOG+="\nTheBench install and set up `date`"
-LOG+="\n======================================================="
-LOG+="\nChecking/installing install requirements"
-echo ""
-echo "TheBench install begins; the result will be logged to $LOGFILE"
-echo ""
-echo "**You may be asked for sudo password"
-echo ""
-packager=
-install=install
-SUDO=sudo  # for Python, SBCL, curl, git install, if need be
-if [[ `command -v yum` ]]; then
-       packager=yum
-       $SUDO yum makecache  # updates the database
-       install="-y install"
-fi
-if [[ `command -v dnf` ]]; then # dnf is more modern than yum, so override if both exists
-       $SUDO dnf copr enable atim/sbcl
-       packager=dnf
-fi
-if [[ `command -v pamac` ]]; then
-       packager=pamac
-fi
-if [[ `command -v yay` ]]; then
-       packager=yay
-       install='-S'
-fi
-if [[ `command -v apt` ]]; then # in case you have both pamac and apt; apt is more likely to spot sbcl
-       packager=apt
-       # open library space of apt and refresh
-       $SUDO add-apt-repository ppa:deadsnakes/ppa # to get older pythons
-       $SUDO apt update
-fi
-if [[ `command -v zypper` ]]; then
-       packager=zypper
-       $SUDO zypper refresh
-fi
-if [[ `command -v brew` ]]; then
-       packager=brew
-       SUDO=                        # brew cannae sudo
-fi
-if [[ $(uname) == "Darwin" ]]; then
-	LOG+="\n  You are using MacOS; I will check brew for install"
-        if [[ `command -v brew` ]]; then
-		LOG+="\n  brew found locally in your computer"
-        else
-                LOG+="\n  installing brew" # MacOS has curl by default
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        fi
-	packager=brew
-        SUDO=                        # brew cannae sudo
-elif [[ $(uname) == "Linux" ]]; then
-	LOG+="\n  You are using Linux; I will use its standard installer ($packager)"
-else
-	LOG+="\n  You are not using Linux, MacOS or Window 11's WSL"
-	if [[ -z $packager ]]; then
-		LOG+="\n  I tried but I cannot locate an installer"
-                LOG+="\n  I CANNOT install TheBench"
-		LOG+="\n**Exiting with error"
-       		echo -e $LOG > $LOGFILE
-       		echo -e $LOG
-       		exit -1
-        else
-		LOG+="\n  I have found an installer though: $packager"
-		LOG+="\n  Using it"
+	#!/bin/bash 
+	# cem bozsahin 2025
+	BENCH_HOMEP="$HOME/.thebenchhome" # this file will contain the pointer to TheBench repo in full path
+	BENCH_HISTORY="$HOME/.thebenchhistory" # bench.py will save commands in it internally
+	BENCHBIN='thebench'  # this is the name of the binary; 'bench' is shorter but its name collides
+	TMPB='/var/tmp/thebench'  # where the temporary files of analysis and training go
+	PY="3.11"                 # specific python for thebench
+	LOGFILE='/var/tmp/thebench-install.log' # goes there to avoid .gitignore in repo directory
+	LOG="========================================================="
+	LOG+="\nTheBench install and set up `date`"
+	LOG+="\n======================================================="
+	LOG+="\nChecking/installing install requirements"
+	echo ""
+	echo "TheBench install begins; the result will be logged to $LOGFILE"
+	echo ""
+	echo "**You may be asked for sudo password"
+	echo ""
+	packager=
+	install=install
+	SUDO=sudo  # for Python, SBCL, curl, git install, if need be
+	if [[ `command -v yum` ]]; then
+	       packager=yum
+	       $SUDO yum makecache  # updates the database
+	       install="-y install"
 	fi
-fi
-if [[ ! -z $packager ]]; then
-	LOG+="\n  Continuing with $packager for installs"
-	if [[ ! -x `command -v curl` ]]; then
-		LOG+="\n  You don't have curl; installing it"
-		$SUDO $packager $install curl
+	if [[ `command -v dnf` ]]; then # dnf is more modern than yum, so override if both exists
+	       $SUDO dnf copr enable atim/sbcl
+	       packager=dnf
+	fi
+	if [[ `command -v pamac` ]]; then
+	       packager=pamac
+	fi
+	if [[ `command -v yay` ]]; then
+	       packager=yay
+	       install='-S'
+	fi
+	if [[ `command -v apt` ]]; then # in case you have both pamac and apt; apt is more likely to spot sbcl
+	       packager=apt
+	       # open library space of apt and refresh
+	       $SUDO add-apt-repository ppa:deadsnakes/ppa # to get older pythons
+	       $SUDO apt update
+	fi
+	if [[ `command -v zypper` ]]; then
+	       packager=zypper
+	       $SUDO zypper refresh
+	fi
+	if [[ `command -v brew` ]]; then
+	       packager=brew
+	       SUDO=                        # brew cannae sudo
+	fi
+	if [[ $(uname) == "Darwin" ]]; then
+		LOG+="\n  You are using MacOS; I will check brew for install"
+		if [[ `command -v brew` ]]; then
+			LOG+="\n  brew found locally in your computer"
+		else
+			LOG+="\n  installing brew" # MacOS has curl by default
+			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		fi
+		packager=brew
+		SUDO=                        # brew cannae sudo
+	elif [[ $(uname) == "Linux" ]]; then
+		LOG+="\n  You are using Linux; I will use its standard installer ($packager)"
 	else
-		LOG+="\n  You have curl; using it"
+		LOG+="\n  You are not using Linux, MacOS or Window 11's WSL"
+		if [[ -z $packager ]]; then
+			LOG+="\n  I tried but I cannot locate an installer"
+			LOG+="\n  I CANNOT install TheBench"
+			LOG+="\n**Exiting with error"
+			echo -e $LOG > $LOGFILE
+			echo -e $LOG
+			exit -1
+		else
+			LOG+="\n  I have found an installer though: $packager"
+			LOG+="\n  Using it"
+		fi
 	fi
-	if [[ ! -x `command -v git` ]]; then
-		LOG+="\n  You don't have git; installig it"
-		$SUDO $packager $install git
+	if [[ ! -z $packager ]]; then
+		LOG+="\n  Continuing with $packager for installs"
+		if [[ ! -x `command -v curl` ]]; then
+			LOG+="\n  You don't have curl; installing it"
+			$SUDO $packager $install curl
+		else
+			LOG+="\n  You have curl; using it"
+		fi
+		if [[ ! -x `command -v git` ]]; then
+			LOG+="\n  You don't have git; installig it"
+			$SUDO $packager $install git
+		else
+			LOG+="\n  You have git; using it"
+		fi
 	else
-		LOG+="\n  You have git; using it"
+	       LOG+="\n  apt, dnf, pamac, yum, zypper or brew not found."
+	       LOG+="\n  Cannot install TheBench"
+	       LOG+="\n**Are you sure you are on a known linux/macOS?"
+	       echo -e $LOG > $LOGFILE
+	       echo -e $LOG
+	       exit -1
 	fi
-else
-       LOG+="\n  apt, dnf, pamac, yum, zypper or brew not found."
-       LOG+="\n  Cannot install TheBench"
-       LOG+="\n**Are you sure you are on a known linux/macOS?"
-       echo -e $LOG > $LOGFILE
-       echo -e $LOG
-       exit -1
-fi
-LOG+="\nDone."
-LOG+="\nChecking/installing Python ($PY), its pip and thebench libs,  and SBCL"
-if [[ ! -z $packager ]]; then
-       if [[ -x `command -v python$PY` ]]; then
-		LOG+="\n  You have python$PY; I will get a local pip for it only"
-       else
-		if [[ $(packager) == "brew" ]]; then
+	LOG+="\nDone."
+	LOG+="\nChecking/installing Python ($PY), its pip and thebench libs,  and SBCL"
+	if [[ ! -z $packager ]]; then
+	       if [[ -x `command -v python$PY` ]]; then
+			LOG+="\n  You have python$PY; I will get a local pip for it only"
+	       else
+			if [[ $(packager) == "brew" ]]; then
                 	LOG+="\n No python$PY; installing it, brew-style"
        			$SUDO $packager $install python"@$PY"
          	else
@@ -174,7 +174,7 @@ chmod u+rw $BENCH_HISTORY
 chmod u+rw $BENCH_HOMEP
 LOG+="\nDone."
 LOG+="\nSetting up thebench binary"
-echo "python$PY $BHF/src/bench.py" > "$HOME/.local/bin/$BENCHBIN" 
+echo "bash -c 'python$PY $BHF/src/bench.py'" > "$HOME/.local/bin/$BENCHBIN" 
 LOG+="\n  TheBench binary thebench is set to execute: `cat $HOME/.local/bin/$BENCHBIN`"
 chmod ugo+x "$HOME/.local/bin/$BENCHBIN"  # to call bench from anywhere
 LOG+="\nDone."
